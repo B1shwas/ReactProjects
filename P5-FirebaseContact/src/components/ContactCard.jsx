cf8e8c328981f7e0ba36d16696f79745a78ee910
Bishwash <bishwash312@gmail.com>
2024-01-09 21:05:25 +0545

    no change

--- P5-FirebaseContact/src/components/ContactCard.jsx
+++ P5-FirebaseContact/src/components/ContactCard.jsx
@@ -7,32 +7,39 @@ import useDisclouse from "../hooks/useDisclouse";
 import AddAndUpdateContact from "./AddAndUpdateContact";
 import { toast } from "react-toastify";
 const ContactCard = ({ contact }) => {
-  const {isOpen, onOpen, onClose} = useDisclouse();
+  const { isOpen, onOpen, onClose } = useDisclouse();
   const deleteContact = async (id) => {
     try {
-      await deleteDoc(doc(db,'contacts',id))
-      toast.success('Successfully Deleted!')
+      await deleteDoc(doc(db, "contacts", id));
+      toast.success("Successfully Deleted!");
     } catch (error) {
       console.log(error);
     }
-  }
+  };
   return (
     <>
-    <div className="bg-yellow flex items-center  justify-between gap-6 rounded-md p-2 ">
-      <div className="flex gap-1">
-        <HiOutlineUserCircle className="text-profile text-5xl" />
-        <div className="">
-          <h1 className="font-medium">{contact.name}</h1>
-          <p className="text-sm ">{contact.email}</p>
+      <div className="flex items-center justify-between  gap-6 rounded-md bg-yellow p-2 ">
+        <div className="flex gap-1">
+          <HiOutlineUserCircle className="text-5xl text-profile" />
+          <div className="">
+            <h1 className="font-medium">{contact.name}</h1>
+            <p className="text-sm ">{contact.email}</p>
+          </div>
+        </div>
+        <div className="flex text-3xl">
+          <RiEditCircleLine className="cursor-pointer" onClick={onOpen} />
+          <IoMdTrash
+            className="cursor-pointer text-purple"
+            onClick={() => deleteContact(contact.id)}
+          />
         </div>
       </div>
-      <div className="flex text-3xl">
-        <RiEditCircleLine className="cursor-pointer" 
-        onClick={onOpen}/>
-        <IoMdTrash className="text-purple cursor-pointer" onClick={()=>deleteContact(contact.id)}/>
-      </div>
-    </div>
-    <AddAndUpdateContact isUpdate isOpen={isOpen} onClose={onClose} contact={contact}/>
+      <AddAndUpdateContact
+        isUpdate
+        isOpen={isOpen}
+        onClose={onClose}
+        contact={contact}
+      />
     </>
   );
 };
